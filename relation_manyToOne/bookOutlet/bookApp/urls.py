3b1e92104ae48fcb601af1ed2bc7a8764b85b11a
akashkewat99 <akashkewat99@gmail.com>
2024-07-27 16:51:35 +0530

    make changes

--- relation_manyToOne/bookOutlet/bookApp/urls.py
+++ relation_manyToOne/bookOutlet/bookApp/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     # path('',views.test,name="test"),
+    path("", views.main, name="HomePage"),
     path('book/',views.books,name="bookHomePage"),
     path('author/',views.author,name="authorsHomePage"),
     path('author/<int:id>/',views.authorDetail,name="authoeDetails"),
--- relation_manyToOne/bookOutlet/bookApp/views.py
+++ relation_manyToOne/bookOutlet/bookApp/views.py
@@ -6,21 +6,28 @@ from . models import Book,Author
 # def test(request):
 #     return HttpResponse("Sucess")
 
+
+def main(request):
+    return render(request,"bookApp/main.html",context=None)
+
 def books(request):
     getAllBooks= Book.objects.all()
     return render(request,'bookApp/index.html',{'getAllBooks':getAllBooks})
 
 def author(request):
     getAllAuthors= Author.objects.all()
+    print("Akash",getAllAuthors)
     return render(request,'bookApp/authors.html',{'getAllAuthors':getAllAuthors})
 
 
 def authorDetail(request,id):
     getAuthorDetail=Author.objects.get(pk=id)
     getAuthorBooks=Book.objects.filter(author=id)
+    print(getAuthorBooks)
     return render(request,"bookApp/authorDetail.html",{'author_name':getAuthorDetail.name,'authorBook':getAuthorBooks})
 
 
 def bookDetails(request,id):
     getBookDetail=Book.objects.get(pk=id)
     return render(request,"bookApp/bookDetails.html",{"Book":getBookDetail})
+
